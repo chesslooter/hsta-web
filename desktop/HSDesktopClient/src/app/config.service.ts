f3c88789ef39a7fc822a0535ede228ba5f847f64
Josh Cordell <jcordell@wisc.du>
2018-03-21 00:40:20 -0500

    correct api call

--- desktop/HSDesktopClient/src/app/config.service.ts
+++ desktop/HSDesktopClient/src/app/config.service.ts
@@ -10,26 +10,26 @@ export class ConfigService {
   constructor(private http: Http) { }
 
   getUserDecklists(userid) {
-    return this.http.get(this.url + '/api/get_user_decklists&userid=' + userid);
+    return this.http.get(this.url + '/api/get_user_decklists?userid=' + userid);
   }
 
   addDeck(userid, deckcode, deckname) {
-    return this.http.get(this.url + '/api/add_deck&userid=' + 
+    return this.http.get(this.url + '/api/add_deck?userid=' + 
             userid + '&deckcode=' + deckcode + '&deckname=' + deckname);
   }
 
   deleteDeck(userid, deckcode) {
-    return this.http.get(this.url + '/api/delete_deck&userid=' + 
+    return this.http.get(this.url + '/api/delete_deck?userid=' + 
             userid + '&deckcode=' + deckcode);
   }
 
   updateDecklistName(userid, deckcode, deckname) {
-    return this.http.get(this.url + '/api/update_decklist_name&userid=' + 
+    return this.http.get(this.url + '/api/update_decklist_name?userid=' + 
             userid + '&deckcode=' + deckcode + '&deckname=' + deckname);
   }
 
   createUser(email) {
-    return this.http.get(this.url + '/api/add_deck&email=' + email);
+    return this.http.get(this.url + '/api/add_deck?email=' + email);
   }
 
 }

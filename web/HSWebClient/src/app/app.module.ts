1683bd3141dd0f0c8924552800dcfddd91a7b5af
jrpaschke <jrpaschke@wisc.edu>
2018-04-09 15:03:47 -0500

    Added components to webclient

--- web/HSWebClient/src/app/app.module.ts
+++ web/HSWebClient/src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TournamentComponent } from './tournament/tournament.component';
+import { TournamentCreateComponent } from './tournament-create/tournament-create.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import {FormsModule} from '@angular/forms';
 
 
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'tournament',
+    component: TournamentComponent
+  },
+  {
+    path: '',
+    component: LandingPageComponent
+  },
+  {
+    path: 'tournamentCreate',
+    component: TournamentCreateComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TournamentComponent,
+    TournamentCreateComponent,
+    NavbarComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    HttpModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
